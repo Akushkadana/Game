1033e72d6f5439fd5e616d72077af0554c387d70
Akushkadana <Akushkadana@gmail.com>
2025-10-26 00:21:05 +0600

    2. Добавлена модель HighScore и админка

--- game/models.py
+++ game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class HighScore(models.Model):
+    player_name = models.CharField(max_length=50, default="Аноним")
+    score = models.IntegerField()
+    played_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-score']
+
+    def __str__(self):
+        return f"{self.player_name} — {self.score}"
