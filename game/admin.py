e199ca338404d6db875761bb6a847a12c1d4bfab
Akushkadana <Akushkadana@gmail.com>
2025-10-26 01:10:14 +0600

    8. Добавлено редактирование рекордов в админке (Update)

--- game/admin.py
+++ game/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import HighScore
 
-admin.site.register(HighScore)
+@admin.register(HighScore)
+class HighScoreAdmin(admin.ModelAdmin):
+    list_display = ('player_name', 'score', 'played_at')
+    list_editable = ('score',)  # ← редактируем только score
+    list_display_links = ('player_name',)  # ← player_name — ссылка на деталь
